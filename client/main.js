4862611557192480d830702121372f2a0263bf0c
Autumn Smith <autumn07@gmail.com>
2016-10-23 17:00:50 -0700

    Added initial files from project. First commit.

---
+++ client/main.js
@@ -0,0 +1,54 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { HTTP } from 'meteor/http'
+
+import './main.html';
+
+var lat, long, data;
+
+Template.latlong.events({
+	//event selection
+	'click button': function(event) {
+		//prevent default submits
+		event.preventDefault();
+
+		// Get value from form element using jQuery id
+		lat = $('#latitude').val();
+		long = $('#longitude').val();
+
+		request();
+
+		//clear form
+		$('#latitude').val('');
+		$('#longitude').val('');
+	}
+});
+
+
+//ajax request to get nearby locations based on lat and long
+var request = function() { return $.get('https://api.instagram.com/v1/locations/search', {
+		access_token: '41731412.e32b74c.33f0932d08db490c95a8c974767fe975',
+		lat: lat,
+		lng: long
+	},
+	function(elem) {
+		$('#list').val('');
+		//get the data array from the get request object
+		console.log(elem['data']);
+		//call the getObj on the data array to append the name and id to the page
+		getObj(elem['data']);
+	}, 'jsonp');
+};
+
+
+var getObj = function(input) {
+	var result = '';
+
+	for (var i = 0, length = input.length; i < length; i++) {
+		result = result.concat('<li>' + input[i]['name'] + ' : ' + input[i]['id'] + ' : ' + '<a href="https://www.instagram.com/explore/locations/' + input[i]['id'] + '">Instagram Link</a>' + '</li>');
+	} 
+	console.log(result);
+
+	$('#list').append(result);
+}
+
---
+++ server/accounts.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.remove({
+  service: 'instagram'
+});
+ServiceConfiguration.configurations.insert({
+  service: 'instagram',
+  scope: 'basic',
+  clientId: 'e32b74cabbd84d2998c50fd105fa5246',
+  secret: '8bffbca9f42d455d98ef9766c4d43777'
+});
+
