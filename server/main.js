53f6093173d6e4ae1354e138b9863d9fc47e439a
Autumn Smith <autumn07@gmail.com>
2016-10-24 21:15:41 -0700

    Added untracked files

--- server/main.js
+++ server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { cheerio } from 'meteor/mrt:cheerio';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+
+Meteor.startup(function() {
+
+	var promise = new Promise( (resolve, reject) => {
+		//Get request to scrape the site
+		HTTP.call('GET', 'https://www.instagram.com/explore/locations/265262816/', function(error, response) {
+			resolve(response.content);
+		})
+	});
+
+	Meteor.methods({
+		getURL: function() {
+			return promise;
+		}
+	})
 });
+
